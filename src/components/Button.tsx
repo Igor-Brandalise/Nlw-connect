b06ca98839b1e4c4becdf21b9ee8e4e672a2f9f9
Igor Brandalise <brandaliseigor123@gmail.com>
2025-02-23 00:39:00 -0300

    button_icon-copy

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,13 @@
-interface ButtonProps
-    children: ReactNode
+import { ReactNode } from "react";
+
+interface ButtonProps {
+  children: ReactNode;
 }
 
-export function Button(props: ButtonProps){
-    return(
-        <button className="px-5 h-12 bg-gray-500 text-blue font-semi-bold rounded-xl w-full cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray900">
-            {props.children}
-        </button>
-    )
+export function Button(props: ButtonProps) {
+  return (
+    <button className="flex justify-between items-center px-5 h-12 bg-gray-500 text-blue font-semi-bold rounded-xl w-full cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray900">
+      {props.children}
+    </button>
+  );
 }
